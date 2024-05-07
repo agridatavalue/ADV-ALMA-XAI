644e398fd3c05cc744b81c389a12da813b3822b0
Mirko Colageo <m.colageo@almaviva.it>
2024-05-07 14:49:36 +0200

    fix swagger and endpoint

--- server/endpoints/BuildEndpoint.py
+++ server/endpoints/BuildEndpoint.py
@@ -1,17 +1,22 @@
+import logging
 from flask import Blueprint, request, jsonify, make_response
 
-buildBp = Blueprint("build", __name__)
-
 from src.adv_xai_fulfilment.presentation.TrainerPresentation import TrainerPresentation
+
+buildBp = Blueprint("build", __name__)
 
 
 @buildBp.route("/build", methods=["POST"])
 def build():
+    if request.method != "POST":
+        return "Not a valid request"
+
+    logging.info(f"called /build api with params {request.get_json()}")
     try:
         response = TrainerPresentation().train(
-            request.form.get("model"),
-            request.form.get("pilot"),
-            request.form.get("metadata"),
+            request.get_json().get("model"),
+            request.get_json().get("pilot"),
+            request.get_json().get("metadata"),
         )
         return make_response(
             jsonify(
