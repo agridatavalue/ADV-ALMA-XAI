d1c9ef8dfdb0328d656ce3cc82d81f67f8f4cfbf
Mirko Colageo <m.colageo@almaviva.it>
2024-11-13 14:21:37 +0100

    created model translator

---
+++ src/adv_xai_fulfilment/domain/service/ModelTranslator.py
@@ -0,0 +1,22 @@
+from ..model.Model import Model
+from ..model.machineLearningModel.KerasModel import KerasModel
+from ..model.machineLearningModel.TorchModel import TorchModel
+from ..model.machineLearningModel.ScikitLearnModel import ScikitLearnModel
+
+
+class ModelTranslator:
+    _framework: str
+    _algorithm: str
+
+    def with_(self, framework: str) -> "ModelTranslator":
+        self._framework = framework
+        return self
+
+    def and_(self, algorithm: str) -> "ModelTranslator":
+        self._algorithm = algorithm
+        return self
+
+    def translate(self, filename: str) -> Model:
+        for model in [KerasModel, TorchModel, ScikitLearnModel]:
+            if self._framework.lower() in model.supported_frameworks():
+                return model(filename)
---
+++ tests/src/domain/service/ModelTranslator_unittest.py
@@ -0,0 +1,30 @@
+import unittest
+
+from src.adv_xai_fulfilment.domain.service.ModelTranslator import ModelTranslator
+from src.adv_xai_fulfilment.domain.model.machineLearningModel.TorchModel import (
+    TorchModel,
+)
+from src.adv_xai_fulfilment.domain.model.machineLearningModel.KerasModel import (
+    KerasModel,
+)
+from src.adv_xai_fulfilment.domain.model.machineLearningModel.ScikitLearnModel import (
+    ScikitLearnModel,
+)
+
+
+class TestModelTranslator(unittest.TestCase):
+    def test_translate(self):
+        testObj = ModelTranslator()
+
+        self.assertIsInstance(
+            testObj.with_("keras").and_("cnn").translate("model.h5"),
+            KerasModel,
+        )
+        self.assertIsInstance(
+            testObj.with_("torch").and_("cnn").translate("model.h5"),
+            TorchModel,
+        )
+        self.assertIsInstance(
+            testObj.with_("scikit-learn").and_("cnn").translate("model.h5"),
+            ScikitLearnModel,
+        )
