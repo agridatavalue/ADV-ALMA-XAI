96b2b067ba86b2db73a17e81399e331602d8e974
Mirko Colageo <m.colageo@almaviva.it>
2024-12-20 09:50:51 +0100

    Aggiunto il servizio MetaDataLoaderService per il caricamento dei metadati degli spiegatori e dei modelli. Aggiornati i servizi QuestionService e ModelPerformanceMetricService per utilizzare il nuovo servizio.

--- src/adv_xai_fulfilment/application/QuestionService.py
+++ src/adv_xai_fulfilment/application/QuestionService.py
@@ -3,6 +3,7 @@ import logging
 from src.adv_xai_fulfilment.infrastructure.Constants import Errors
 from src.adv_xai_fulfilment.domain.model.questions.Feedback import Feedback
 from src.adv_xai_fulfilment.domain.model.questions.Question import Question
+from ..infrastructure.service.MetaDataLoaderService import MetaDataLoaderService
 from src.adv_xai_fulfilment.domain.model.ExplainerMetaData import ExplainerMetaData
 from src.adv_xai_fulfilment.domain.model.ExplainerIdentifier import ExplainerIdentifier
 from src.adv_xai_fulfilment.infrastructure.service.DataLoaderService import (
@@ -15,10 +16,12 @@ from src.adv_xai_fulfilment.infrastructure.service.ExplainerRepositoryService im
 
 class QuestionService:
     _data_loader_service: DataLoaderService
+    _metadata_loader_service: MetaDataLoaderService
     _explainer_repository_service: ExplainerRepositoryService
 
     def __init__(self):
         self._data_loader_service = DataLoaderService()
+        self._metadata_loader_service = MetaDataLoaderService()
         self._explainer_repository_service = ExplainerRepositoryService()
 
     def generate_from_dict(self, expl_id: ExplainerIdentifier) -> list[Question]:
@@ -27,7 +30,7 @@ class QuestionService:
 
         logging.debug(f"loading metadata from {expl_id.metadata}")
         meta_data: ExplainerMetaData = (
-            self._data_loader_service.load_explainer_metadata(expl_id)
+            self._metadata_loader_service.load_explainer_metadata(expl_id)
         )
 
         return [q.verticalize_for(meta_data.model_metadata) for q in Question.get_all()]
@@ -38,7 +41,7 @@ class QuestionService:
 
         logging.debug(f"loading metadata from {feedback.explainer_identifier.metadata}")
         meta_data: ExplainerMetaData = (
-            self._data_loader_service.load_explainer_metadata(
+            self._metadata_loader_service.load_explainer_metadata(
                 feedback.explainer_identifier
             )
         )
---
+++ src/adv_xai_fulfilment/infrastructure/service/MetaDataLoaderService.py
@@ -0,0 +1,72 @@
+import os
+import json
+import pandas as pd
+from os import path
+
+from ...domain.model.ModelMetaData import ModelMetaData
+from ..repository.BucketRepository import BucketRepository
+from ...domain.model.ExplainerMetaData import ExplainerMetaData
+from src.adv_xai_fulfilment.infrastructure.Constants import Errors
+from .translator.ModelMetaDataTranslator import ModelMetaDataTranslator
+from .translator.ExplainerMetaDataTranslator import ExplainerMetaDataTranslator
+from src.adv_xai_fulfilment.domain.model.ExplainerIdentifier import ExplainerIdentifier
+
+
+class MetaDataLoaderService:
+    _bucketRepository: BucketRepository
+    _model_metadata_translator: ModelMetaDataTranslator
+    _explainer_metadata_translator: ExplainerMetaDataTranslator
+
+    def __init__(self):
+        self._bucketRepository = BucketRepository(
+            {
+                "endpoint": os.getenv("MINIO_ENDPOINT"),
+                "access_key": os.getenv("MINIO_ACCESS_KEY"),
+                "secret_key": os.getenv("MINIO_SECRET_KEY"),
+                "secure": os.getenv("MINIO_SECURE", "true").lower() == "true",
+            }
+        )
+        self._model_metadata_translator = ModelMetaDataTranslator()
+        self._explainer_metadata_translator = ExplainerMetaDataTranslator()
+
+    def load_file(self, file_path: str, bucket_name: str) -> pd.DataFrame:
+        file: str = self._bucketRepository.download_from(
+            object_name=file_path,
+            bucket_name=bucket_name,
+        )
+        return pd.read_csv(file)
+
+    def load_explainer_metadata(
+        self, expl_id: ExplainerIdentifier
+    ) -> ExplainerMetaData:
+        assert isinstance(
+            expl_id, ExplainerIdentifier
+        ), Errors.EXPLAINER_IDENTIFIER_NOT_EXPLAINER_IDENTIFIER
+
+        file: str = self._bucketRepository.download_from(
+            object_name=expl_id.get_metadata_path(),
+            bucket_name=os.getenv("EXPLAINER_FOLDER_PATH"),
+        )
+        with open(file, "r") as json_file:
+            metadata = json.load(json_file) or {}
+
+        os.remove(file)
+        return self._explainer_metadata_translator.translate(metadata)
+
+    def load_model_metadata(self, expl_id: ExplainerIdentifier) -> ModelMetaData:
+        assert isinstance(
+            expl_id, ExplainerIdentifier
+        ), Errors.EXPLAINER_IDENTIFIER_NOT_EXPLAINER_IDENTIFIER
+
+        file: str = expl_id.get_metadata_locale_filepath()
+        if not path.exists(file):
+            file: str = self._bucketRepository.download_from(
+                object_name=expl_id.metadata.filename.lower(),
+                bucket_name=os.getenv("MODEL_FOLDER_PATH"),
+                destination_file_path=file,
+            )
+
+        with open(file, "r") as json_file:
+            metadata = json.load(json_file) or {}
+
+        return self._model_metadata_translator.translate(metadata)
