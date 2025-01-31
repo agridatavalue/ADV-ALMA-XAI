433bb2c30c233025f8a99c62c8fd421b3ac60466
Mirko Colageo <m.colageo@almaviva.it>
2025-01-31 10:12:37 +0100

    tests all clear

--- tests/src/application/ModelPerformanceMetricService_unittest.py
+++ tests/src/application/ModelPerformanceMetricService_unittest.py
@@ -82,12 +82,21 @@ class TestModelPerformanceMetricService(unittest.TestCase):
     @patch(
         "src.adv_xai_fulfilment.application.ModelPerformanceMetricService.ModelPerformanceServiceComponent"
     )
+    @patch(
+        "src.adv_xai_fulfilment.application.ModelPerformanceMetricService.MetaDataLoaderService"
+    )
     def test_get_metrics(
-        self, MockMPMService, MockModelLoader, MockDataLoader, mock_getenv
+        self,
+        MockDataLoader,
+        MockMPMService,
+        MockModelLoader,
+        ModelMetaDataService,
+        mock_getenv,
     ):
         mock_data_loader = MockDataLoader.return_value
         mock_mpm_service = MockMPMService.return_value
         mock_model_loader = MockModelLoader.return_value
+        mock_metadata_loader_service = ModelMetaDataService.return_value
 
         mock_model = MagicMock(spec=Model)
         mock_model_metadata = MagicMock(spec=ModelMetaData)
@@ -104,8 +113,16 @@ class TestModelPerformanceMetricService(unittest.TestCase):
         mock_model_metadata.index_of_target_name.return_value = 1
         mock_data_loader.load_model_metadata.return_value = mock_model_metadata
         mock_mpm_service.get_metrics.return_value = {"precision": 0.85}
+        mock_metadata_loader_service.load_model_metadata.return_value = (
+            mock_model_metadata
+        )
 
         service = ModelPerformanceMetricService()
+        service._metadata_loader_service = mock_metadata_loader_service
+        service._model_loader_service = mock_model_loader
+        service._data_loader_service = mock_data_loader
+        service._mdm_service = mock_mpm_service
+
         result = service.get_metrics(explainer_identifier)
 
         self.assertEqual(result["precision"], 0.85)
