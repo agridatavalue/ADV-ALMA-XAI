f9dfbd537904ff3066b0445134a8ef3caa8c3583
Mirko Colageo <m.colageo@almaviva.it>
2025-03-19 10:10:35 +0100

    fix for empty prediction_targets in build api

--- src/adv_xai_fulfilment/presentation/ExplainerGeneratorPresentation.py
+++ src/adv_xai_fulfilment/presentation/ExplainerGeneratorPresentation.py
@@ -1,26 +1,44 @@
 import logging
 
+from ..domain.model.partner import Partner
 from .validator import ExplainerGeneratorValidator
 from .translator import ExplainerIdentifierTranslator
+from ..domain.model.model_metadata import ModelMetaData
 from ..domain.model.explainers.explainer import Explainer
 from ..domain.model.explainer_identifier import ExplainerIdentifier
 from ..domain.model.explainers.response_data import ExplainerResponseData
 from ..application.explainer_generator_service import ExplainerGeneratorService
+from ..infrastructure.service.MetaDataLoaderService import MetaDataLoaderService
 
 
 class ExplainerGeneratorPresentation:
     _validator: ExplainerGeneratorValidator
     _translator: ExplainerIdentifierTranslator
     _build_service: ExplainerGeneratorService
+    _metadata_loader_service: MetaDataLoaderService
 
     def __init__(self):
         self._validator = ExplainerGeneratorValidator()
         self._translator = ExplainerIdentifierTranslator()
         self._build_service = ExplainerGeneratorService()
+        self._metadata_loader_service = MetaDataLoaderService()
 
     def build(self, data: dict = {}) -> list[Explainer]:
         logging.info(f"called build with params: {data}")
         self._validator.validate_and_sanitize_build(data)
+
+        if not data.get('prediction_targets'):
+            model_metadata: ModelMetaData = self._metadata_loader_service.load_model_metadata(
+                expl_id=ExplainerIdentifier(
+                    prediction_target = "",
+                    data = data.get("data"),
+                    model = data.get("model"),
+                    partner = Partner(data.get("partner")),
+                    metadata_identifier = data.get('meta_data'),
+                )
+            )
+            data['prediction_targets'] = model_metadata.target_names
+
         requests: list[ExplainerIdentifier] = self._translator.translate_many(data)
 
         explainers: list[list[Explainer]] = [
