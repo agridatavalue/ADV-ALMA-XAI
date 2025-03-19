785b3d652f5a24c24c61ccd769d71fed7dc95254
Mirko Colageo <m.colageo@almaviva.it>
2025-03-19 09:00:43 +0100

    fix test

--- tests/src/infrastructure/service/ModelLoaderService_unittest.py
+++ tests/src/infrastructure/service/ModelLoaderService_unittest.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, patch
 
 from src.adv_xai_fulfilment.domain.model.model import Model
 from src.adv_xai_fulfilment.domain.model.machine_learning_model import KerasModel
+from src.adv_xai_fulfilment.domain.model.explainer_identifier import ExplainerIdentifier
 from src.adv_xai_fulfilment.infrastructure.service.ModelLoaderService import (
     ModelLoaderService,
 )
@@ -29,7 +30,7 @@ class ModelLoaderServiceTest(unittest.TestCase):
         with open("test.json", "w") as f:
             f.write('{"framework": "test", "algorithm": "test"}')
 
-        actual = testObj.load_from("test.json", MagicMock())
+        actual = testObj.load_from(ExplainerIdentifier(model='test.json'), MagicMock())
 
         os.remove("test.json")
 
