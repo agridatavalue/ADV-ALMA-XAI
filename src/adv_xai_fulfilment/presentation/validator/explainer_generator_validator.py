0d0a693e4defd98a31828f3ac575128647c4d2eb
Mirko Colageo <m.colageo@almaviva.it>
2025-03-20 14:47:55 +0100

    make build data param as required

--- src/adv_xai_fulfilment/presentation/validator/explainer_generator_validator.py
+++ src/adv_xai_fulfilment/presentation/validator/explainer_generator_validator.py
@@ -5,6 +5,7 @@ class ExplainerGeneratorValidator(AbstractValidator):
     def validate_and_sanitize_build(self, data: dict) -> dict:
         self._validate_model(data.get("model"))
         self._validate_partner(data.get("partner"))
+        self._validate_folder_data(data.get("data"))
         return data
 
     def validate_and_sanitize_prepare(self, data: dict) -> dict:
