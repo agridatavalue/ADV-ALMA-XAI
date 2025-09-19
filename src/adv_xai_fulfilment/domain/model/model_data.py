9ef89c968c3e518b630d248da5ebe0663fccd132
Mirko Colageo <m.colageo@almaviva.it>
2025-09-19 14:53:44 +0200

    clean

--- src/adv_xai_fulfilment/domain/model/model_data.py
+++ src/adv_xai_fulfilment/domain/model/model_data.py
@@ -35,7 +35,40 @@ class ModelData:
         self.data_train = pd.DataFrame()
         self.data_predict = pd.DataFrame()
         self.data_validation = pd.DataFrame()
-    
+        
+    def calculate_x_and_y_predict_and_x_and_y_train(
+        self, 
+        model: "Model",
+        feature_names: list[str], 
+        model_category: ModelCategory,
+        target_name: str = "",
+    ) -> "ModelData":
+        if self.data_predict is not None and not self.data_predict.empty:            
+            cols_to_remove = [col for col in self.data_predict.columns if col not in feature_names]
+            if cols_to_remove:
+                logger.debug(f"predict - Removing columns not in feature names: {cols_to_remove}")
+                self._x_predict = self.data_predict.drop(columns=cols_to_remove)
+                self._x_predict = self._x_predict[feature_names]
+                
+        if self.data_train is not None and not self.data_train.empty:
+            cols_to_remove = [col for col in self.data_train.columns if col not in feature_names]
+            X = self.data_train.drop(columns=cols_to_remove)
+            y = self.data_train[target_name]
+            if ModelCategory.is_classification(str(model_category)):
+                _, X_test, _, y_test = train_test_split(X, y, test_size=0.25, stratify=y, random_state=42)
+            else:
+                _, X_test, _, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+            self._y_test = y_test
+            self._y_predict = model.handler.predict(X_test)
+            
+            self._y_train = self.data_train[target_name] if target_name in self.data_train.columns else self.data_train
+            if cols_to_remove:
+                logger.debug(f"train - Removing columns not in feature names: {cols_to_remove}")
+                self._x_train = self.data_train.drop(columns=cols_to_remove)
+                self._x_train = self._x_train[feature_names]
+                
+        return self
+        
     # --------------------------------------------------------------
     @property
     def y_test(self) -> pd.DataFrame:
@@ -93,55 +126,12 @@ class ModelData:
         assert isinstance(image, str)
         self._image_path = image
 
-    def get_y_for_prediction_target(self, target: int) -> pd.Series:
-        if self._y_predict is None or self._y_predict.empty:
-            return pd.Series(dtype=float)
-        return self._y_predict.iloc[:, target]
-
-    def y_train_is_empty(self) -> bool:
-        return self._y_train is None or self._y_train.empty
-    
     @property
     def is_empty(self) -> bool:
         return self.data_predict is None or self.data_train is None or (self.data_predict.empty and self.data_train.empty)
 
     def __repr__(self) -> str:
         return f"ModelData(predict_x={self._x_predict}, predict_y={self._y_predict}, image_path={self._image_path})"
-
-    # --------------------------------------------------------------
-    
-    def calculate_x_and_y_predict_and_x_and_y_train(
-        self, 
-        model: "Model",
-        feature_names: list[str], 
-        model_category: ModelCategory,
-        target_name: str = "",
-    ) -> "ModelData":
-        if self.data_predict is not None and not self.data_predict.empty:            
-            cols_to_remove = [col for col in self.data_predict.columns if col not in feature_names]
-            if cols_to_remove:
-                logger.debug(f"predict - Removing columns not in feature names: {cols_to_remove}")
-                self._x_predict = self.data_predict.drop(columns=cols_to_remove)
-                self._x_predict = self._x_predict[feature_names]
-                
-        if self.data_train is not None and not self.data_train.empty:
-            cols_to_remove = [col for col in self.data_train.columns if col not in feature_names]
-            X = self.data_train.drop(columns=cols_to_remove)
-            y = self.data_train[target_name]
-            if ModelCategory.is_classification(str(model_category)):
-                _, X_test, _, y_test = train_test_split(X, y, test_size=0.25, stratify=y, random_state=42)
-            else:
-                _, X_test, _, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-            self._y_test = y_test
-            self._y_predict = model.handler.predict(X_test)
-            
-            self._y_train = self.data_train[target_name] if target_name in self.data_train.columns else self.data_train
-            if cols_to_remove:
-                logger.debug(f"train - Removing columns not in feature names: {cols_to_remove}")
-                self._x_train = self.data_train.drop(columns=cols_to_remove)
-                self._x_train = self._x_train[feature_names]
-                
-        return self
     
     # --------------------------------------------------------------
     
@@ -165,6 +155,3 @@ class ModelData:
         """DEPRECATED, use y_predict instead"""
         self._y_predict = y
     
-    def y_is_empty(self) -> bool:
-        """DEPRECATED, use y_predict_is_empty instead"""
-        return self._y_predict is None or self._y_predict.empty
