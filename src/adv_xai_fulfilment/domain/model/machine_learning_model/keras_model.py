1e5c804f4dee558320ab50a0ac5c0a0667488058
Mirko Colageo <m.colageo@almaviva.it>
2025-10-06 15:51:54 +0200

    made consistent get_feature_importance method in keras

--- src/adv_xai_fulfilment/domain/model/machine_learning_model/keras_model.py
+++ src/adv_xai_fulfilment/domain/model/machine_learning_model/keras_model.py
@@ -20,16 +20,17 @@ class KerasModel(Model):
         return self.handler.predict(X)
 
     def get_feature_importance(
-        self, feature_names: list[FeatureDescription], shap_values: np.array
+        self, feature_names: list[FeatureDescription], shap_values: np.ndarray
     ) -> pd.DataFrame:
         mean_abs_shap_values = np.mean(np.abs(shap_values), axis=0)
-        return pd.DataFrame(
-            [
-                {
-                    "Feature": [f.name for f in feature_names],
-                    "Importance": mean_abs_shap_values.tolist(),
-                }
-            ]
+
+        return (
+            pd.DataFrame({
+                "Feature": [f.name for f in feature_names],
+                "Importance": mean_abs_shap_values.flatten(),
+            })
+            .sort_values(by="Importance", ascending=False)
+            .reset_index(drop=True)
         )
 
     def get_partial_dependence(
